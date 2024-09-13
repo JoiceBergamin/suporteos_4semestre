809249a6ab48551c40256015bd3245ebb0075307
Joice Bergamin <joicenafef@gmail.com>
2024-09-13 10:25:48 -0300

    Projeto Base Java + Spring Boot  concluído

---
+++ suporteos/src/main/java/com/curso/resources/ProdutoResource.java
@@ -0,0 +1,25 @@
+package com.curso.resources;
+
+import com.curso.domains.dtos.ProdutoDTO;
+import com.curso.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping (value = "/produto")
+public class ProdutoResource {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping //exemplo: localhost:8080/produto
+    public ResponseEntity<List<ProdutoDTO>> findAll(){
+        return ResponseEntity.ok().body(produtoService.findAll());
+
+    }
+}
