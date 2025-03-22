4d1d36dfb45fd53f8e55463041638c1811d3eafc
Joice Bergamin <joicenafef@gmail.com>
2025-03-21 22:08:42 -0300

    Commit 17: Criando as Repositories para Person, User, Technician e ServiceOrder

---
+++ suporteos/src/main/java/com/curso/repositories/TechnicianRepository.java
@@ -0,0 +1,9 @@
+package com.curso.repositories;
+
+import com.curso.domains.Technician;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TechnicianRepository extends JpaRepository<Technician, Long> {
+}
